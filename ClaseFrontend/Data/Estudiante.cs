edb067031f10a0bea7d2a69437d5e49d2430474d
Gabrieljimenez1 <63116950+Gabrieljimenez1@users.noreply.github.com>
2022-05-27 14:13:34 -0500

    Add files via upload

---
+++ ClaseFrontend/Data/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClaseFrontend.Data
+{
+    public class Estudiante
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Carrera { get; set; }
+        public string Semestre { get; set; }
+        public int Prestamos { get; set; }
+    }
+}

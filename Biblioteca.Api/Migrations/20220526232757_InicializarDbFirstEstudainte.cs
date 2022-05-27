49cba0b129eb1d7719a911529004800b78857ea6
Gabriel David Jimenez Mosquera <gabrieljimo@unisabana.edu.co>
2022-05-27 14:10:15 -0500

    Agregar archivos de proyecto.

---
+++ Biblioteca.Api/Migrations/20220526232757_InicializarDbFirstEstudainte.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Api.Migrations
+{
+    public partial class InicializarDbFirstEstudainte : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Libros");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Libros",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

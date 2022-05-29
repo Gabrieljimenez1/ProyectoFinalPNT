0bd7e41e7dfd67ed9e2eb3847fac3078c8ec969a
Gabrieljimenez1 <63116950+Gabrieljimenez1@users.noreply.github.com>
2022-05-28 19:07:07 -0500

    Add files via upload

---
+++ ClaseFrontend/Data/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClaseFrontend.Data
+{
+    public class Estudiante
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string carrera { get; set; }
+        public string semestre { get; set; }
+        public int prestamos { get; set; }
+    }
+}
---
+++ ClaseFrontend/Data/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClaseFrontend.Data
+{
+    public class Libro
+    {
+        [Key]
+        public int idlibro { get; set; }
+        public string titulo { get; set; }
+        public string editorial { get; set; }
+        public string autor { get; set; }
+        public int pagina { get; set; }
+    }
+}
